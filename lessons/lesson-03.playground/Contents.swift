88976defea84d56f250edd1147e0a5da8dbe85b4
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2024-12-11 00:35:50 +0100

    01 - Lesson 3 - Variables

---
+++ lessons/lesson-03.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var count1 = 10.0
+var count2: Int = 10
+var count3: Double = 10.0
+var count4: Float = 10.0
+
+let multiline = """
+Hello world
+Next line
+"""
+
+print(multiline)
+
+let name = "John"
+let greetingWithName = "Hello \(name)"
+
+print(greetingWithName)
