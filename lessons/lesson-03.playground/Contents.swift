84fa555adecdc137f916e9d01b87523063b749b7
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2024-12-11 23:30:20 +0100

    02 - Lesson 3 - Variable - AI Trainer Tasks

--- lessons/lesson-03.playground/Contents.swift
+++ lessons/lesson-03.playground/Contents.swift
@@ -17,3 +17,40 @@ let name = "John"
 let greetingWithName = "Hello \(name)"
 
 print(greetingWithName)
+
+var optionalString: String? = nil
+var myBool = true
+print(myBool)
+
+var numberA:Int = 5
+var numberB:Double = 2.5
+var sum = Double(numberA) + numberB
+print(sum)
+
+var nubmerToCheck = 10
+var message = nubmerToCheck % 2 == 0 ? "Число четное" : "Число нечетное"
+print(message)
+
+var optionalNumber : Int? = nil
+if let value = optionalNumber {
+    print(value)
+}
+
+
+let multilineOne = """
+Это первая строка
+Это вторая строка
+Это третья строка
+"""
+print(multilineOne)
+
+var isAlive = true
+print(isAlive)
+
+//var integerValue : Int = 42
+//var doubleValue : Double = Double(integerValue)
+//print(doubleValue)
+
+var doubleValue : Double = 42.0
+var messageDoubleValue = doubleValue > 50 ? "Больше 50" : "Не больше 50"
+print(messageDoubleValue)
